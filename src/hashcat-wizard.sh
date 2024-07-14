15ad7df90a3f74be239caf7e5181a6a01f45062f
bgropay <bgropay@gmail.com>
2024-07-14 08:50:43 +0700

    Update hashcat-wizard.sh

--- src/hashcat-wizard.sh
+++ src/hashcat-wizard.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 # [ hashcat-wizard.sh ]
+
+# tentang program 
+program="hashcat-wizard"
+deskripsi="Crack hash secara otomatis menggunakan Hashcat."
+pembuat="bgropay"
+versi="1.0.0"
+github="https://github.com/bgropay/hashcat-wizard/"
+
+# fungsi selamat datang 
+function selamatDatang(){
+    echo ""
+    echo "Selamat datang di ${program}"
+    echo ""
+}
+
+# fungsi hashcat-wizard
+function hashcatWizard(){
+  selamatDatang 
+}
+
+hashcatWizard
